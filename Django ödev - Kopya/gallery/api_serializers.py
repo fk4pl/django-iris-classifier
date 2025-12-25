b0717df110166dbaad256789772d702582aa7b80
Adem Özyılmaz <adom.4101@gmail.com>
2025-12-25 17:52:07 +0300

    Klasör yapısı düzenlendi

--- Django ödev - Kopya/gallery/api_serializers.py
+++
@@ -1,8 +0,0 @@
-from rest_framework import serializers
-from .models import IrisSample
-
-
-class IrisSampleSerializer(serializers.ModelSerializer):
-    class Meta:
-        model = IrisSample
-        fields = ['id', 'instance_id', 'sepal_length', 'sepal_width', 'petal_length', 'petal_width', 'species']
--- Django ödev - Kopya/gallery/api_views.py
+++
@@ -1,8 +0,0 @@
-from rest_framework import viewsets
-from .models import IrisSample
-from .api_serializers import IrisSampleSerializer
-
-
-class IrisSampleViewSet(viewsets.ModelViewSet):
-    queryset = IrisSample.objects.all()
-    serializer_class = IrisSampleSerializer
